1ed6d11562b2d93eb43693cc7a308fd6263dd5fd
deelaka99 <123wejith@gmail.com>
2023-09-18 11:32:29 +0530

    added layout to the dashboard component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Dashboard() {
   return (
-    <div className=''>
-      dashboard
+    <div className="p-5 h-full w-full">
+      <div className="p-2 w-full h-1/2">
+        <div className="h-full w-full rounded-md shadow-md border-2 border-white"></div>
+      </div>
+      <div className="flex p-2 w-full h-1/2">
+        <div className="h-full w-3/4 rounded-md shadow-md border-2 border-white"></div>
+        <p>&nbsp;&nbsp;&nbsp;</p>
+        <div className="h-full w-1/4 rounded-md shadow-md border-2 border-white"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
