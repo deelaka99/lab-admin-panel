6e793866960d3c72b45c38125e6ea82aaf8bed75
deelaka99 <123wejith@gmail.com>
2023-09-21 17:56:04 +0530

    added dark theme to searchbar and add user btn+dow

--- src/components/UserManagement/ManageUsers.jsx
+++ src/components/UserManagement/ManageUsers.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
-import SearchBar from '../searchbar/SearchBar';
+import React from "react";
+import SearchBar from "../searchbar/SearchBar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileArrowDown, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 
 function ManageUsers() {
   return (
-    <div className='flex flex-col w-full h-full'>
-      <div className='flex w-full h-1/6'>
-        <div className='flex justify-center items-center h-full w-1/2'>
-          <SearchBar/>
+    <div className="flex flex-col w-full h-full">
+      <div className="flex w-full h-1/6">
+        <div className="flex justify-center items-center h-full w-1/2">
+          <SearchBar />
         </div>
-        <div className='bg-bermuda h-full w-1/2'></div>
+        <div className="flex items-center justify-end h-full w-1/2 p-5">
+          <button className="p-2 border border-ternary-blue text-ternary-blue text-lg rounded shadow-md hover:shadow-lg hover:text-white hover:border-white dark:text-gray2 dark:border-gray2 dark:shadow-black dark:hover:text-gray1 dark:hover:border-gray1">
+            <FontAwesomeIcon icon={faFileArrowDown} />
+          </button>
+          <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
+          <button className="p-2 border border-ternary-blue font-inter bg-primary-blue text-ternary-blue text-lg rounded shadow-md hover:shadow-lg hover:text-white hover:border-white dark:bg-black dark:bg-opacity-50 dark:text-gray2 dark:border-gray2 dark:shadow-black dark:hover:text-gray1 dark:hover:border-gray1">
+            <FontAwesomeIcon icon={faUserPlus} />
+            &nbsp;&nbsp;Add New User
+          </button>
+        </div>
       </div>
-      <div className='bg-red w-full h-5/6'></div>
+      <div className="bg-red w-full h-5/6"></div>
     </div>
-  )
+  );
 }
 
-export default ManageUsers
+export default ManageUsers;
--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -6,10 +6,11 @@ function SearchBar() {
             <div className="flex rounded">
                 <input
                     type="text"
-                    className="placeholder:text-primary-blue font-inter font-regular block w-full px-4 py-2 text-primary-blue bg-ternary-blue border border-primary-blue rounded-md focus:border-secondary-blue focus:ring-ternary-blue focus:outline-none focus:ring focus:ring-opacity-40"
+                    className="shadow-md placeholder:text-primary-blue font-inter font-regular block w-full px-4 py-2 text-primary-blue bg-ternary-blue border border-primary-blue rounded-md focus:border-secondary-blue focus:ring-ternary-blue focus:outline-none focus:ring focus:ring-opacity-40 dark:bg-dark-ternary dark:text-gray2 dark:placeholder:text-gray2 dark:border-gray2 dark:focus:border-gray1 dark:focus:ring-primary-blue dark:focus:ring-opacity-30 dark:shadow-black"
                     placeholder="Search..."
                 />
-                <button className="px-4 text-ternary-blue bg-primary-blue border border-ternary-blue shadow-lg rounded ">
+                <p>&nbsp;</p>
+                <button className="shadow-md px-4 text-ternary-blue bg-primary-blue border border-ternary-blue hover:shadow-lg hover:text-white rounded dark:bg-black dark:bg-opacity-50 dark:border-gray2 dark:text-gray2 dark:shadow-black dark:hover:text-gray1 dark:hover:border-gray1">
                     Search
                 </button>
             </div>
